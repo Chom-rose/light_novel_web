2dcb85486ad0d04c91e6060965255b3592b14f80
Jompol CHUENRAROM <221150542+Chom-rose@users.noreply.github.com>
2025-09-11 05:02:38 +0700

    อัพเดต
    เหนื่อยแล้วนะ ส่งพน.อะไร

--- controller/novel.js
+++ controller/novel.js
@@ -8,6 +8,38 @@ exports.read = async (req, res) => {
     }
 };
 
+exports.read_create = async (req, res) => {
+    try {
+        res.sendFile(path.join(__dirname, "../views/create.html"));
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+exports.read_write = async (req, res) => {
+    try {
+        res.sendFile(path.join(__dirname, "../views/write.html"));
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+exports.read_write_chapter = async (req, res) => {
+    try {
+        res.sendFile(path.join(__dirname, "../views/write_chapter.html"));
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+exports.read_search = async (req, res) => {
+    try {
+        res.sendFile(path.join(__dirname, "../views/search.html"));
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
 
 exports.create = async (req, res) => {
     try {
--- router/novel.js
+++ router/novel.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const path = require("path");
 // controller
 
-const { read, create } = require("../controller/novel");
+const { read, create, read_create,  read_write ,read_write_chapter, read_search} = require("../controller/novel");
 
 // routes
 router.get("/", read);
 router.post("/create", create);
-
- 
-
+router.get("/create", read_create);
+router.get("/write", read_write);
+router.get("/write_chapter", read_write_chapter); 
+router.get("/search", read_search);
 
 module.exports = router;
