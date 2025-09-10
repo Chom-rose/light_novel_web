ec1e36865a0c767908889cff2f9eeb54ae7eaf02
Wipoosana50 <wipoosana.p@ku.th>
2025-09-10 17:14:24 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,18 +7,14 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-<<<<<<< HEAD
-// const adminRouter = require("./router/admin");
-const lightNovelRouter = require("./router/novel");
-
-// app.use("/admin", adminRouter);
-=======
 const adminRouter = require("./router/admin");
 const lightNovelRouter = require("./router/novel");
 
 app.use("/admin", adminRouter);
->>>>>>> 626a386e48e7e1c81cc649e0acae38601e8e38fb
 app.use("/light-novel", lightNovelRouter);
+
+// redirect root -> หน้าเว็บหลัก
+app.get("/", (req, res) => res.redirect("/light-novel"));
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
