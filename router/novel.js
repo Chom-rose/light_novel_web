1bfc6bd803b6a68edd14f02dd39a106b3b17e711
Wipoosana50 <wipoosana.p@ku.th>
2025-09-11 18:44:50 +0700

    Update novel.js

--- router/novel.js
+++ router/novel.js
@@ -5,6 +5,11 @@ const router = express.Router();
 const {
   mainPage,
   createPage,
+  read_write,
+  read_write_chapter,
+  read_search,
+  read_login,
+  read_register,
   novelPage,
   listNovels,
   getNovel,
@@ -18,6 +23,11 @@ const { authRequired } = require("../controller/auth");
 // Pages แก้ตรงนี้ด้วย
 router.get("/", mainPage);
 router.get("/create", createPage);
+router.get("/write", read_write);
+router.get("/write_chapter", read_write_chapter);
+router.get("/search", read_search);
+router.get("/login", read_login);
+router.get("/register", read_register);
 // router.get("/novel", novelPage); // expects ?id=xxx (novel id)
 
 // API Novels
