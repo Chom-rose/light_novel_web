67f45fde6497549a05cecec4a51de078a1696d57
Wipoosana50 <wipoosana.p@ku.th>
2025-09-15 22:45:48 +0700

    Update novelView.js

--- router/novelView.js
+++ router/novelView.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const novelView = require("../controller/novelView");
 const authOptional = require("../middleware/authOptional");
-const chapter = require("../controller/chapter");
+// const chapter = require("../controller/chapter");
 
 // View routes
 router.get("/write/:id", novelView.renderWrite);
 router.get("/write_chapter/:id", novelView.renderWriteChapter);
 router.get("/novel/:id",authOptional, novelView.renderNovelDetail);
-router.get("/novel/:id/chapter/:chapterId",authOptional, chapter.readChapter);
+// router.get("/novel/:id/chapter/:chapterId",authOptional, chapter.readChapter);
+router.get("/novel/:novelId/chapter/:chapterId", authOptional, novelView.readChapterPage);
 
 module.exports = router;
